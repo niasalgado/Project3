6fb03c1d8da8b878e8e78e17a1357c6549dedf1c
niasalgado <salgado.nia@gmail.com>
2024-04-18 11:34:16 -0400

    init commit with class files

---
+++ Demo.java
@@ -0,0 +1,25 @@
+// void upheap(int j) {
+//     while(j > 0) {
+//         int p = parent(j);
+//         if (compare(heap.get(j), heap.get(p)) >= 0)
+//             break;
+//         swap(j, p);
+//         j = p;
+//     }
+// }
+
+// void downheap(int j) {
+//     while (hasLeft(j)) {
+//     ...
+//     }
+// }
+
+
+// removeMin() {
+//     if (heap.isempty())
+//         return null;
+//     Entry<K, V> answer = heap.get();
+//     swap(0, heap.size() - 1);
+//     ...
+// }
+
---
+++ Entry.java
@@ -0,0 +1,14 @@
+/*******************************************************
+ *  @Author : Ali Azhari   
+ *  Created On : Wed Apr 01 2024
+ *  @File : Entry.java
+ *  Description: An abstract base class to 
+ * assist implementations of the PriorityQueue interface
+ *******************************************************/
+
+/** Interface for a key-value pair. */
+public interface Entry<K,V> { 
+    K getKey( ); // returns the key stored in this entry
+    V getValue( ); // returns the value stored in this entry
+    }
+    
